64fd2daea3e9a0586f31232f73dc0b766002993d
Bianca Gomes <105321115+Bibi568@users.noreply.github.com>
2022-12-29 17:04:59 -0400

    Feat: project creation functionality

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -17,4 +17,13 @@ Cypress.Commands.add('login', (
     cy.get('.qa-user-avatar').click()
     cy.contains('Sign out').click()
   })
+
+  Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('/projects/new')
+  
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get('.qa-initialize-with-readme-checkbox').check()
+    cy.contains('Create project').click()
+  })
   
