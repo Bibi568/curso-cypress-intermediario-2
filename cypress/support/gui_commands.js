a303348e5869ebc7c697ded80f982a75c3025f73
Bianca Gomes <105321115+Bibi568@users.noreply.github.com>
2022-12-29 17:12:37 -0400

    Feat: save session

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,17 +1,33 @@
 Cypress.Commands.add('login', (
-    user = Cypress.env('user_name'),
-    password = Cypress.env('user_password'),
-  ) => {
-    const login = () => {
-      cy.visit('/users/sign_in')
-  
-      cy.get("[data-qa-selector='login_field']").type(user)
-      cy.get("[data-qa-selector='password_field']").type(password, { log: false })
-      cy.get("[data-qa-selector='sign_in_button']").click()
-    }
-  
+  user = Cypress.env('user_name'),
+  password = Cypress.env('user_password'),
+  { cacheSession = true } = {},
+) => {
+  const login = () => {
+    cy.visit('/users/sign_in')
+
+    cy.get("[data-qa-selector='login_field']").type(user)
+    cy.get("[data-qa-selector='password_field']").type(password, { log: false })
+    cy.get("[data-qa-selector='sign_in_button']").click()
+  }
+
+  const validate = () => {
+    cy.visit('/')
+    cy.location('pathname', { timeout: 1000 })
+      .should('not.eq', '/users/sign_in')
+  }
+
+  const options = {
+    cacheAcrossSpecs: true,
+    validate,
+  }
+
+  if (cacheSession) {
+    cy.session(user, login, options)
+  } else {
     login()
-  })
+  }
+})
 
   Cypress.Commands.add('logout', () => {
     cy.get('.qa-user-avatar').click()
